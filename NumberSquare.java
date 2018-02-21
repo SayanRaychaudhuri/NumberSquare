78d1d2b2b4b72d9acede19fe6c597d7954919d07
Sayan Raychaudhuri <Raychaudhurisayan@gmail.com>
2018-02-21 00:16:50 -0500

    Major code simplification

--- NumberSquare.java
+++ NumberSquare.java
@@ -1,27 +1,19 @@
 import java.util.*;
+
 public class NumberSquare {
 
-	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.print("Min? ");
-	String min = input.nextLine();
-	System.out.print("Max? ");
-	String max = input.nextLine();
-	int Min = Integer.valueOf(min);
-	int Max = Integer.parseInt(max);
-	String numString = "";
-	for (int i=Min;i<=Max;i++) {
-		numString += String.valueOf(i);
-	}
-	if (Max%10==0) {
-	for (int j=0; j<=numString.length()-2;j++){
-		System.out.println(numString.substring(j, numString.length())+numString.substring(0, j));
-			}
-		} else {
-	for (int j=0; j<=numString.length()-1;j++){
-		System.out.println(numString.substring(j, numString.length())+numString.substring(0, j));
-			}		
-		}
-	}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Min? ");
+        int min = input.nextInt();
+        System.out.print("Max? ");
+        int max = input.nextInt();
+        String numString = "";
+        for (int i = min; i <= max; i++) {
+            numString += String.valueOf(i);
+        }
+            for (int j = 0; j < numString.length() - 1; j++) {
+                System.out.println(numString.substring(j) + numString.substring(0, j));
+            }
+    }
 }
-
